1f19d0f292b2644a2befa8f3fd55c1dbf606b175
yarntime <yarntime@163.com>
2018-07-19 17:06:54 +0800

    remove insecure config

--- pkg/operator/config.go
+++ pkg/operator/config.go
@@ -23,7 +23,7 @@ func NewRedisOperatorConfig() *Config {
 // AddFlags add cobra flags to populate Config
 func (c *Config) AddFlags(fs *pflag.FlagSet) {
 	fs.StringVar(&c.KubeConfigFile, "kubeconfig", c.KubeConfigFile, "Location of kubecfg file for access to kubernetes master service")
-	fs.StringVar(&c.Master, "master", "192.168.254.44:8080", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
+	fs.StringVar(&c.Master, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
 	fs.StringVar(&c.ListenAddr, "addr", "0.0.0.0:8086", "listen address of the http server which serves kubernetes probes and prometheus endpoints")
 	c.Redis.AddFlags(fs)
 }
